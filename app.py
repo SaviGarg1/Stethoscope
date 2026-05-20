f9e72d6a0777c6b8acc6bed7c4e12da7cd5cacf7
SaviGarg1 <isavigarg@gmail.com>
2026-05-20 14:44:40 -0400

    add libraries

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return """
-    <h1>Welcome to MedTalk</h1>
-    <p>This is my first healthcare website.</p>
-    """
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
